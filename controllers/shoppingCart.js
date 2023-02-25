8f5330cf27e687cf8ba0eb792b8349f301396ae4
Karina Garces ✨ <karinadgarces@gmail.com>
2023-02-25 12:36:11 -0500

    get error

--- controllers/shoppingCart.js
+++ controllers/shoppingCart.js
@@ -6,8 +6,8 @@ const localStorage = new LocalStorage('./scratch');
 
 
 // add a product to the users shopping cart
-
 const addToCart = async (req, res) => {
+    //localStorage.clear()
     // need to provide backend with productId
     // create an add to cart button passing product._id
     // POST form to /cart 
@@ -53,7 +53,6 @@ const addToCart = async (req, res) => {
         if(existingCartItemIndex !== -1){
             // add price func here
             
-
         } else {
             console.log("EC INSIDE ELSE"+existingCartItemIndex)
             cart.items.push({
@@ -79,13 +78,57 @@ const addToCart = async (req, res) => {
 
 
 // remove a product from the users shopping cart
-
 const removeFromCart = async (req, res) => {
-}
+        try {
+            const productId = req.params.productId;
+            const userId = localStorage.getItem('user');
+           
+            if (!localStorage.getItem('user')) {
+                const guestUser = new User({
+                    name: 'Guest',
+                    email: 'guest@example.com',
+                  });
+                  await guestUser.save();
+                  localStorage.setItem('user', guestUser._id);
+            }
+    
+            const product = await Products.findById(productId);
+    
+            if(!product){
+                return res.status(404).json({message:"Product not found"});
+            }
+    
+            if (!userId) {
+                return res.status(404).json({message:"User not found"});
+            }
+            
+            let cart = JSON.parse(localStorage.getItem('cart'));
+            if (!cart) {
+                cart = {
+                    user: userId,
+                    items: [],
+                };
+            }
+    
+            const existingCartItemIndex = cart.items.findIndex(item => item.productId === productId);
+    
+            if(existingCartItemIndex !== -1){
+                cart.items.splice(existingCartItemIndex, 1);
+                localStorage.setItem('cart', JSON.stringify(cart));
+                return res.redirect('/cart');
+            } else {
+                return res.status(404).json({message:"Item not found in cart"});
+            }
+            
+        }
+        catch(err){
+            console.error(err);
+            res.status(500).json({message:"server error"})
+        }
+    };
+
 
 // get the users shopping cart
-
-
 const getCart = async (req, res) => {
 }
 
--- routes/carts.js
+++ routes/carts.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const cartCtrl = require('../controllers/ShoppingCart.js');
 
-
+//router.get('/cart', cartCtrl.getCart);
 router.post('/cart', cartCtrl.addToCart);
-
+router.delete('/cart/delete/:productId', cartCtrl.removeFromCart);
 
 module.exports = router;
