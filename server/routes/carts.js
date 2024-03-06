0ee42f429623b5eaa7279846aef6482f8b21a87f
Karina Garces ✨ <karinadgarces@gmail.com>
2024-03-06 02:37:02 -0500

    Shopping cart (#41)
    * get error
    
    * remove from cart functional
    
    * get cart functional
    
    * removed shoppingcart controller import
    
    * route fix import
    
    * update .ignore
    
    * remove
    
    * scratch

--- server/routes/carts.js
+++ server/routes/carts.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const cartCtrl = require('../controllers/ShoppingCart.js');
+const cartCtrl = require('../controllers/shoppingCart.js');
 
 
 router.post('/cart', cartCtrl.addToCart);
-
+router.get('/cart', cartCtrl.getCart);
+router.get('/cart/delete/:productId', cartCtrl.removeFromCart);
 
 module.exports = router;
